8bbdcce2e7d4d98fc3917ad0b39a7399b589b714
Oluwajoba <oluwajobabello@gmail.com>
2020-02-11 08:08:13 +0100

    subit task 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+print(open("Intro-Python-I/src/foo.txt", "r").read())
+print(open("Intro-Python-I/src/foo.txt").close())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+# print(open("Intro-Python-I/src/bar.txt", "w"))
+print(open("Intro-Python-I/src/bar.txt", "w").write("lknsjnbslnebnebnlbs \n onwbooegoto \n nobotooitotor"))
+print(open("Intro-Python-I/src/bar.txt").close())
+print(open("Intro-Python-I/src/bar.txt", "r").read())
+print(open("Intro-Python-I/src/bar.txt").close())
